43538c1728a4615fc085aa206d619e393064f129
Ameer Abbas <ameer00@gmail.com>
2024-10-17 15:44:02 -0700

    renamed helper files, added standard handlers

--- backend_handlers.py
+++ backend_handlers.py
@@ -7,6 +7,27 @@ app = Flask(__name__)
 
 session_maker = SessionMaker()
 
+# standard http handlers
+@app.route('/discovery', methods=['GET'])
+def discovery():
+    return jsonify({
+        "name": "shipping",
+        "version": "1.0",
+        "owners": ["ameerabbas", "janedoe"],
+        "organization": "acme"
+    })
+
+# create a /liveness and /readiness endpoints that return status, code and timestamps
+@app.route('/liveness', methods=['GET'])
+def liveness():
+    return jsonify({"status": "live", "code": 200, "timestamp": "2023-04-27T12:00:00Z"})
+
+@app.route('/readiness', methods=['GET'])
+def readiness():
+    return jsonify({"status": "ready", "code": 200, "timestamp": "2023-04-27T12:00:00Z"})
+# End of standard http handlers
+
+# get a package from CloudSQL database
 # Endpoint that retrieves package details based on the provided product ID
 @app.route('/packages/<int:product_id>', methods=['GET'])
 def get_package(product_id):
@@ -33,6 +54,7 @@ def get_package(product_id):
     else:
         abort(404, description="The product_id was not found")
 
+# create a new package in the CloudSQL database
 # Endpoint that creates a new package in the database.
 @app.route('/packages', methods=['POST'])
 def create_package():
@@ -66,7 +88,6 @@ def create_package():
         )
         session.add(new_package)
         session.commit()
-        # session.close()
 
         return jsonify({"package_id": new_package.id}), 201
     except KeyError as e:
@@ -74,6 +95,7 @@ def create_package():
     except ValueError as e:
         abort(400, description=f"Invalid data: {e}")
 
+# update an existing package in the CloudSQL database
 # Endpoint that updates an existing package in the database.
 @app.route('/packages/<int:package_id>', methods=['PUT'])
 def update_package(package_id):
@@ -119,6 +141,7 @@ def update_package(package_id):
     else:
         abort(404, description="The package_id was not found")
 
+# delete a package in the CloudSQL database
 # Endpoint that deletes an existing package from the database.
 @app.route('/packages/<int:package_id>', methods=['DELETE'])
 def delete_package(package_id):
@@ -145,6 +168,6 @@ def delete_package(package_id):
         abort(404, description="The package_id was not found")
 
 
-
+# Run the app
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
---
+++ vars.sh
@@ -0,0 +1,6 @@
+export INSTANCE_CONNECTION_NAME=duet01:us-central1:postgres
+export DB_USER=evolution
+export DB_PASS=evolution
+export DB_NAME=shipping
+export PROJECT_ID=duet01
+gcloud config set project duet01
